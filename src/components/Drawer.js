ab8fb505bd39699b0871b691b5a652dd33cbc928
Anna-Grigencha <grigencha10@gmail.com>
2024-02-11 14:23:05 +0300

    feat: add lesson4

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,29 +1,23 @@
-function Drawer() {
+function Drawer({ onClose, items = [] }) {
   return (
-    <div style={{ display: 'none' }} className="overlay">
+    <div className="overlay">
       <div className="drawer">
-        <h2 className="mb-30 d-flex justify-between">Корзина <img className="cu-p" src="/img/btn-remove.svg" alt="Remove" /></h2>
+        <h2 className="mb-30 d-flex justify-between">Корзина
+          <img onClick={onClose} className="cu-p" src="/img/btn-remove.svg" alt="Close" /></h2>
 
 
         <div className="items">
 
-          <div className="cartItem d-flex align-center mb-20">
-            <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }} className="cartItemImg"></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Мужские кроссовки Nike Blazer Mid Suede</p>
-              <b>12 999 руб.</b>
+          {items.map((obj) => (
+            <div className="cartItem d-flex align-center mb-20">
+              <div style={{ backgroundImage: `url(${obj.imageUrl})` }} className="cartItemImg"></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">{obj.title}</p>
+                <b>{obj.price}</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
             </div>
-            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
-          </div>
-
-          <div className="cartItem d-flex align-center mb-20">
-            <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }} className="cartItemImg"></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Мужские кроссовки Nike Blazer Mid Suede</p>
-              <b>12 999 руб.</b>
-            </div>
-            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
-          </div>
+          ))}
         </div>
 
 
